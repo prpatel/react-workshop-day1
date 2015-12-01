306945133e503f793e5e190720fe8de3160fd882
pratik patel <pratik@mypatelspace.com>
2015-12-01 08:54:43 -0500

    forgot to git add some stuff

---
+++ test/todo-item-test.js
@@ -0,0 +1,29 @@
+var React = require('react/addons'),
+    assert = require('assert'),
+    TodoItem = require('../Lab13/todo-item'),
+    TestUtils = React.addons.TestUtils;
+
+describe('Todo-item component', function(){
+  before('render and locate element', function() {
+    var renderedComponent = TestUtils.renderIntoDocument(
+      <TodoItem done={false} name="Write Tutorial"/>
+    );
+
+    // Searching for <input> tag within rendered React component
+    // Throws an exception if not found
+    var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
+      renderedComponent,
+      'input'
+    );
+
+    this.inputElement = inputComponent.getDOMNode();
+  });
+
+  it('<input> should be of type "checkbox"', function() {
+    assert(this.inputElement.getAttribute('type') === 'checkbox');
+  });
+
+  it('<input> should not be checked', function() {
+    assert(this.inputElement.checked === false);
+  });
+});
